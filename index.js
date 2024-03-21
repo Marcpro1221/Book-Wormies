b03699a71bc430d9edfce96f9c5eb7a0bb522e5e
Marc Roland Soniega <marcrolandmarc@gmail.com>
2024-03-21 10:51:22 +0800

    sidebar changes

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-
 var center = document.querySelector(".center");
 var sidebar = document.querySelector(".sidebar");
-var headerList = document.querySelectorAll(".sidebar .header");
+var headerList = document.querySelectorAll(".sidebar .hidden-item");
+var mediaQuery  = window.matchMedia('(min-width: 561px)');
+
+
 function toggleNavigation() {
       center.classList.toggle("clicked");
       sidebar.classList.toggle("sidebar-open");
 
-    headerList.forEach(function (item) {
+    headerList.forEach(function (item) {  
         item.style.display = (sidebar.classList.contains("sidebar-open")) ? "block" : "none";
     });
-    
+
     if (sidebar.style.width === "0px" || sidebar.style.width === "") {
-        sidebar.style.width = "300px";
+        sidebar.style.width = "250px";
     } else {
         sidebar.style.width = "0";
     }
 }
+window.addEventListener('resize', function(){
+    headerList.forEach(function (item){
+        item.style.display = mediaQuery.matches ? "block" : (sidebar.classList.contains("sidebar-open") ? "block" : "none");
+    });
+})
+
+
+
 
